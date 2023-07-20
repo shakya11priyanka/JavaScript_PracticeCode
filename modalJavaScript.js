4c71984f25404f07bb4519207e96c354b21ddcb4
shakya11priyanka <66562198+shakya11priyanka@users.noreply.github.com>
2023-07-20 05:11:58 -0700

    Add files via upload

---
+++ modalJavaScript.js
@@ -0,0 +1,60 @@
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnOpenModal=document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+const openModal = function(){
+  console.log('Button clicked');
+    modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for(let i=0;i<btnOpenModal.length;i++){
+  btnOpenModal[i].addEventListener('click',openModal);
+}
+
+const closeModal= function(){
+
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+
+};
+
+btnCloseModal.addEventListener('click',closeModal);
+
+overlay.addEventListener('click',closeModal);
+
+
+
+
+// 'use strict';
+//
+// const modal = document.querySelector('.modal');
+// const overlay = document.querySelector('.overlay');
+// const btnCloseModal = document.querySelector('.close-modal');
+// const btnsOpenModal = document.querySelectorAll('.show-modal');
+//
+// const openModal = function () {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// };
+//
+// const closeModal = function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// };
+//
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+//
+// btnCloseModal.addEventListener('click', closeModal);
+// overlay.addEventListener('click', closeModal);
+//
+// document.addEventListener('keydown', function (e) {
+//   // console.log(e.key);
+//
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     closeModal();
+//   }
+// });
